1d6ec54a022a7e48f9428f7c21b7908f282bd317
vtjaeger <viniciustjaeger@gmail.com>
2024-04-09 15:34:48 -0300

    created project

---
+++ src/test/java/br/patrimony/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.patrimony.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
