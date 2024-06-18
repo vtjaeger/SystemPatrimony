94442ee15b139b1fae749ebaa6e9e18538c0ea63
vtjaeger <viniciustjaeger@gmail.com>
2024-06-18 11:24:08 -0300

    more time for the token

--- src/main/java/br/patrimony/system/security/TokenService.java
+++ src/main/java/br/patrimony/system/security/TokenService.java
@@ -46,6 +46,6 @@ public class TokenService {
     }
 
     public Instant expirationDate(){
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(4).toInstant(ZoneOffset.of("-03:00"));
     }
 }
